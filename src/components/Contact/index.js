698459ab8dd277a8bfc0d29fae389a7975f76e30
Robert Schmahl <rschm007@gmail.com>
2021-02-15 19:41:29 -0800

    contact front end almost complete. Uploading resume to complete download href

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,51 @@
-import CTAbutton from "../CTAbutton";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileDownload } from "@fortawesome/free-solid-svg-icons";
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+import ContactForm from "../ContactForm";
 import LineBreak from "../LineBreak";
 
 const styles = {
-    title: "title justify-center",
-  };
+  title: "title text-white justify-center",
+  h2: "text-5xl tracking-tight font-bold mt-7",
+  h3: "text-3xl tracking-tight font-medium pb-7 mt-5",
+  h4: "text-2xl text-pink-800 tracking-tight font-medium pb-7",
+  icons:
+    "text-6xl -mt-8 mb-6 m-3 outerGlowborder-transparent hover:text-pink-500 hover:border-pink-700 transition duration-200 ease-in-out transform hover:-translate-y-1",
+  paragraph: "text-lg pb-6 leading-7 text-black",
+};
 
 const Contact = (props) => {
   return (
-    <div className="contact flex w-full h-full pt-20 -mt-28 pb-28 mb-28 px-80 justify-center">
-                <div className={styles.title}>
-          <h2 className={styles.h2}>My name is Robert</h2>
+    <div className="contact flex w-full h-full pt-20 -mt-28 pb-28 mb-28 px-80 justify-center pt-40 z-60">
+      <div className="wrapper bg-black opacity-90 px-20 py-2">
+        <div className={styles.title}>
+          <h2 className={styles.h2}>Let's start a conversation</h2>
           <LineBreak />
+          <div className="social-icons relative">
+            <a
+              href="https://github.com/rschm007"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <a
+                href="../../../assets/downloads/2020-Resume=RobertSchmahl.pdf"
+                download="2020-Resume-RobertSchmahl.pdf"
+              >
+                <FontAwesomeIcon
+                  icon={faFileDownload}
+                  className={styles.icons}
+                />
+              </a>
+
+              <FontAwesomeIcon icon={faGithubSquare} className={styles.icons} />
+              <FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
+            </a>
+          </div>
+          <ContactForm />
         </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/ContactForm/index.js
@@ -0,0 +1,42 @@
+import CTAbutton from "../CTAbutton";
+
+const styles = {
+    formGroup: "form-group flex flex-wrap -mx-3 mb-6",
+    label: "block uppercase tracking-wide text-gray-100 text-lg font-bold my-2 mr-2",
+    inputField: "form-control appearance-none block w-full bg-gray-300 text-gray-100 border border-gray-500 rounded-sm py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-200",
+}
+
+const ContactForm = () => {
+  return (
+    <form
+      id="contact-form"
+    //   onSubmit={this.handleSubmit.bind(this)}
+      method="POST"
+      className="w-full max-w-lg"
+    >
+      <div className={styles.formGroup}>
+        <label htmlFor="name" className={styles.label} >Name</label>
+        <input type="text" className={styles.inputField} type="text" />
+      </div>
+      <div className={styles.formGroup}>
+        <label className={styles.label} htmlFor="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          className={styles.inputField}
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label className={styles.label} htmlFor="message">Message</label>
+        <textarea className={styles.inputField} rows="5"></textarea>
+      </div>
+      <CTAbutton
+            href="#"
+            text="Submit"
+            styles="mt-1 mb-5 ml-5 text-center bg-pink-500 py-4 px-8 text-2xl text-white uppercase font-bold border-2 border-transparent hover:bg-transparent hover:border-pink-700 transition duration-200 ease-in-out transform hover:-translate-y-1"
+          />
+    </form>
+  );
+};
+
+export default ContactForm;
